a8a251e90ace105646d440ce5d434bbf5f2e328f
Jerry Xiong <xion0597@umn.edu>
2021-04-25 14:27:57 -0500

    worked on stretch goals 1-3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,15 +1,37 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-const basket = []
+let basket = []
+const maxItems = 5
+
 console.log(`My starting basket contents: ${basket}`);
 
+
+function isFull(){
+  if (basket.length >= maxItems) {
+    return true;
+  } else if (basket.lenth < maxItems) {
+    return false;
+  } else {
+    return false;
+  }
+} // end full
+
+console.log(`Basket is full?: ${isFull()}`); // testing isFull
+
+
 function addItem( item ){
-  basket.unshift( item );
-  return true;
+  if ( isFull() ){
+    console.log('basket is full');
+    return false;
+  } else if (basket.length < maxItems) {
+    basket.unshift( item );
+    return true;
+  }
 } // end addItem
 
 console.log( 'Picked up some bananas:', addItem('bananas') );
+
 
 function listItems(){
   for (item in basket){
@@ -27,6 +49,7 @@ console.log(`Basket contents: ${basket}`);
 
 console.log('I empty my basket at checkout');
 
+
 function empty(){
   for (let i = 0; basket.length = 0; i++) {
     basket.shift(i);
@@ -37,3 +60,13 @@ function empty(){
 empty();
 
 console.log(`Basket contents: ${basket}`);    // testing empty
+
+
+addItem( 'beef');
+addItem( 'chicken');
+addItem( 'fish');
+addItem( 'pork');
+addItem( 'beans');
+addItem( 'corn'); // testing isFull false outcome
+
+console.log(`Basket contents: ${basket}`);
