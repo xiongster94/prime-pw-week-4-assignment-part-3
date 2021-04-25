c670025ef43f44234f6e9d39d68d1644ebc6c0da
Jerry Xiong <xion0597@umn.edu>
2021-04-25 13:32:19 -0500

    made some log changes, made listItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,11 +1,26 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = []
+const basket = []
+console.log(`Basket contents: ${basket}`);
 
 function addItem( item ){
   basket.unshift( item );
   return true;
-} //end addItem
+} // end addItem
 
-console.log( 'running addItem:', addItem('bananas') );
+console.log( 'Picked up some bananas:', addItem('bananas') );
+
+function listItems(){
+  for (item in basket){
+    console.log('I have some:', basket[ item ]);
+  }
+  return;
+} // end listItems
+
+addItem( 'apples' );
+addItem( 'pears' );
+
+listItems();    //testing listItems
+
+console.log(`Basket contents: ${basket}`);
