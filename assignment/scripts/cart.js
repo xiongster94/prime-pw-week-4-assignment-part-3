e64973bcc4ec58e51947fc0bedcd9c2260c3b905
Jerry Xiong <xion0597@umn.edu>
2021-04-25 13:47:09 -0500

    made empty function with working log

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,7 +2,7 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 const basket = []
-console.log(`Basket contents: ${basket}`);
+console.log(`My starting basket contents: ${basket}`);
 
 function addItem( item ){
   basket.unshift( item );
@@ -24,3 +24,16 @@ addItem( 'pears' );
 listItems();    //testing listItems
 
 console.log(`Basket contents: ${basket}`);
+
+console.log('I empty my basket at checkout');
+
+function empty(){
+  for (let i = 0; basket.length = 0; i++) {
+    basket.shift(i);
+  }
+  return;
+} // end empty
+
+empty();
+
+console.log(`Basket contents: ${basket}`);    // testing empty
