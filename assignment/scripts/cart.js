9301638618929865503b15073a6ac77538cc5882
Jerry Xiong <xion0597@umn.edu>
2021-04-25 12:58:42 -0500

    made addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,3 +2,10 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = []
+
+function addItem( item ){
+  basket.unshift( item );
+  return true;
+} //end addItem
+
+console.log( 'running addItem:', addItem('bananas') );
